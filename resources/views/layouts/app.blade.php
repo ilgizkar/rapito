2f602d24b632fecfe9bfafac4e8f2b15008cbc74
Ilgiz <karabaev.il@yandex.ru>
2021-03-20 16:34:12 +0300

    First

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
+    <script src="https://vk.com/js/api/xd_connection.js?2"  type="text/javascript"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
